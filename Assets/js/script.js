ccfef42c0854b22c7d21cb96b0b1c2542eb7d6df
Gulam Faruk <golamfaruk204@gmail.com>
2021-11-16 15:53:39 +0600

    design part done

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,36 +1,35 @@
-
 $(document).ready(function(){
-
-  //=============================
-    // Sticky Start
-    //=============================
-
-    $(window).on("scroll", function (event) {
-        var scroll = $(window).scrollTop();
-        if (scroll < 200) {
-            $(".fixed-menu").removeClass("sticky");
-        } else {
-            $(".fixed-menu").addClass("sticky");
-        }
-    });
-        //=============================
-    // MOBILE Nav
-    //=============================
-
-    $(".navbar-toggler").on("click", function () {
-        $(".main-nav").slideToggle(500);
-    });
-
-    $('.owl-carousel').owlCarousel({
-        margin:10,
-        loop:true,
-        autoWidth:true,
-        items:4
-    });
-
-    $(".search-icon-bar a").click(function(){
-        $(".search-popup").toggle(500);
+ 
+    $('.slider').slick({
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 3,
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 3
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 1
+            }
+          }
+        ]
       });
 
-  });
+
+
+
+
+
+});
  
